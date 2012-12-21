9d083559d67d9e724f748bd47da06d9d02d1d772
Laurent Prodon <laurent@sitronnier.com>
2012-12-21 18:13:21 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from distutils.core import setup
 
 setup(
     name = 'lst',
-    version = '0.0.2',
+    version = '0.0.4',
     packages = ['lst'],
     description = 'Liip Scrum Toolbox',
     author = 'sitron',
     author_email = 'laurent@sitronnier.com',
     url = 'https://github.com/sitron/lst',
+    scripts = ['bin/lst'],
+    package_data = {'lst': ['html_templates']},
 )
 
