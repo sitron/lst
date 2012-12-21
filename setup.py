d0552b370cb2cc9d60ddc6d798a5491fcb2c86ae
Laurent Prodon <laurent@sitronnier.com>
2012-12-21 17:34:45 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -9,5 +9,14 @@ setup(
     author = 'sitron',
     author_email = 'laurent@sitronnier.com',
     url = 'https://github.com/sitron/lst',
+    requires = [
+        'PyYAML>=3.10',
+        'argparse>=1.2.1',
+        'python-dateutil>=2.1',
+        'six>=1.2.0',
+        'wsgiref>=0.1.2'
+    ],
+    scripts = ['bin/lst'],
+    package_data = {'lst': ['html_templates']},
 )
 
