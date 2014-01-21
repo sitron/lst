59c5cee29427488f2477d65e58a3cb6d4134bc28
Laurent Prodon <laurent@sitronnier.com>
2014-01-21 22:15:27 +0100

    add requirements in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 from setuptools import setup, find_packages
 from lst import __version__
 
+
 setup(
     name='lst',
     version=__version__,
@@ -12,5 +13,13 @@ setup(
     url='https://github.com/sitron/lst',
     scripts=['bin/lst'],
     test_suite="lst.tests.suite",
+    install_requires=[
+        'PyYAML>=3.10',
+        'argparse>=1.2.1',
+        'python-dateutil>=2.1',
+        'six>=1.2.0',
+        'pygal>=1.1.0',
+        'beautifulsoup4>=4.3.2',
+    ],
 )
 
